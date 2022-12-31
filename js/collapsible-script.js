8b8edf49f1a17867a5321765e8400c400af1fd47
papa <visidoor@yandex.ru>
2022-12-31 12:20:41 +0500

    feat: make ArrowUp = Shift + Tab

--- js/collapsible-script.js
+++ js/collapsible-script.js
@@ -154,7 +154,7 @@ document.addEventListener("keydown", function (e) {
         showFocus();
         if (e.shiftKey) {
           fromA();
-          gotoPrevSibling();
+          gotoPrevSiblingOrCousin(hasParentLi);
         } else {
           fromA();
           gotoNextSibling();
